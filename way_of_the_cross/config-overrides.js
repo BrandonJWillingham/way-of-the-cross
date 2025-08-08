7855d7bcf71c3210f49f3b35f379b66243f1b94d
BrandonJWillingham <BrandonJWillingham@gmail.com>
2025-08-08 00:51:11 -0400

    config override

--- way_of_the_cross/config-overrides.js
+++ way_of_the_cross/config-overrides.js
@@ -3,8 +3,8 @@ const path = require("path");
 module.exports = function override(config, env) {
   config.resolve.alias = {
     ...config.resolve.alias,
-    react: path.resolve(__dirname, "./node_modules/react"),
-    "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+    react: require.resolve("react"),
+    "react-dom": require.resolve("react-dom"),
   };
   return config;
 };
